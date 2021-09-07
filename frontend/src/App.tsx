7a2fea7f0b8ce58eb605deb5b839718e876a454c
nycolas-vieira <nycolasvieira@hotmail.com>
2021-09-07 11:48:29 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,34 @@
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import BarChart from 'components/Charts/BarChart';
+import DonutChart from 'components/Charts/DonutChart';
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 
 function App() {
   return (
     <>
       <NavBar />
 
-      <div className="app-container">
-        <h1 className='text-primary'>Hello world</h1>
+      <div className='app-container'>
+        <h1 className='text-primary py-3'>Dashboard de Vendas</h1>
+
+        <div className='row px-3'>
+          <div className='col-sm-6'>
+            <h5 className='text-center text-secondary'>Taxa de sucesso</h5>
+
+            <BarChart />
+          </div>
+
+          <div className='col-sm-6'>
+            <h5 className='text-center text-secondary'>Todas as vendas</h5>
+
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className='py-3'>
+          <h2 className='text-primary'>Todas as vendas</h2>
+        </div>
 
         <DataTable />
       </div>
--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,6 +1,18 @@
-import TBody from "./tbody";
-
 const DataTable = () => {
+    const TBody = () => {
+        return (
+            <>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Barry Allen</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+            </>
+        );
+    }
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
