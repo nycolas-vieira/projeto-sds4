14280cfa53e49500364867db3c06d34bb158442f
nycolas-vieira <nycolasvieira@hotmail.com>
2021-09-10 00:50:24 -0300

    create repositories and packages

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,4 +1,9 @@
 package com.devsuperior.dsvendas.repositories;
 
-public interface SaleRepository {
+import com.devsuperior.dsvendas.repositories.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Long, Sale> {
 }
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,4 +1,9 @@
 package com.devsuperior.dsvendas.repositories;
 
-public interface SellerRepository {
+import com.devsuperior.dsvendas.repositories.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Long, Seller> {
 }
